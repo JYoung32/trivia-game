b8b2375232e377a0f71211f2653290cdf880b4b8
Joseph Young <joseph.young32@yahoo.com>
2019-09-20 14:19:33 -0400

    minor styling adjustments, created psuedocode for logic

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+//create a variable to store questions
+//create a variable to store possible choices for each question
+//manipulate the page to show chosen question with possible choices
+//if user clicks right answer, store count and move on to next question
+//if user clicks wrong answer, store count, display correct answer, and move on to next question
+//if user does not choose an answer within timer, count as inccorect answer, display answer, and move on to next question
+//at the end of the game, display scoreboard of correct answer and wrong answers.
