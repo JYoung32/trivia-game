e14009f61f2e31c661e02ab362a81b8906437875
Joseph Young <joseph.young32@yahoo.com>
2019-09-22 15:08:37 -0400

    countdown timer working, next question with choices will display after timer reaches 0

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,93 +1,110 @@
-//create a variable to store questions
-//create a variable to store possible choices for each question
+//create a variable to store questions and to store possible choices for each question
 //manipulate the page to show chosen question with possible choices
 //if user clicks right answer, store count and move on to next question
 //if user clicks wrong answer, store count, display correct answer, and move on to next question
 //if user does not choose an answer within timer, count as unanswered, display answer, and move on to next question
 //at the end of the game, display scoreboard of correct answer and wrong answers.
 
-
-var trivia = {
-    //variable for trivia
-    correct: 0,
-    incorrect: 0,
-    unanswered: 0,
-    currentSet: 0,
-    timer: 15,
-    timerOn: false,
-    timerCount: '',
-    //questions
-    questions: ["What superhero is Tony Stark?", "What is Batman's alias?", "Which superhero is not part of the Fantastic 4?", "Which newspaper does Peter Parker work for?", "Wolverine's claws are made of", "What item is not part of Batman's aresenal?", "What Superhero team includes a sentient tree creature?", "Question8", "Question9", "Question10"],
-    //choices
-    choices: [
-        ["Batman", "Spider-Man", "Iron Man", "Deadpool"],
-        ["Tony Stark", "Bruce Wayne", "Clark Kent", "Steven Strange"],
-        ["The Hulk", "Mr. Fantastic", "Human Torch", "Susan Storm"],
-        ["New Daily Globe", "The Daily Planet", "The Daily News", "The Daily Bugle"],
-        ["Titanium", "Adamantium", "Vibranium", "Aluminum"],
-        ["Batmobile", "Grappling Hook", "Power Ring", "Utility Belt"],
-        ["Teen Titans", "Guardians Of The Galaxy", "Suicide Squad", "The Justice League"],
-        ["a", "b", "c", "d"],
-        ["a", "b", "c", "d"],
-        ["a", "b", "c", "d"],
-    ],
-    //answers
-    answers: ["Iron Man", "Bruce Wayne", "The Hulk", "The Daily Bugle", "Adamantium", "Power Ring", "Guardians Of The Galaxy", "Answer8", "Answer9", "Answer10"],
-}
-
-$(document).ready(function(){
-//make a function to put question into questions class with a 15 second timer
-//function ask question with choices
-
-//JS to insert items, will be moved later
-$(".question").html(trivia.questions[0]);
-$(".btn-1").html(trivia.choices[0][0]);
-$(".btn-2").html(trivia.choices[0][1]);
-$(".btn-3").html(trivia.choices[0][2]);
-$(".btn-4").html(trivia.choices[0][3]);
-$(".timer").html(trivia.timer + " Seconds")
-
-
-//timeout counter
-
-//function to start game
-function gamestart(){
-    //game start values
-    trivia.currentSet = 0;
-    trivia.correct = 0;
-    trivia.incorrect = 0;
-    trivia.unanswered = 0;
-
-    $(".timer").text(trivia.timer + " Seconds")
-
-}
-//function to ask question
-function askQuestion(){
-    //reset timer for question
-    trivia.timer = 15;
-    $(".timer").text(trivia.timer + " Seconds");
-    //prevent speed up
-    if(!trivia.timerOn){
-        setInterval(timerRun, 1000);
+//variable for trivia
+var trivia = [
+    {
+        question: "What superhero is Tony Stark?",
+        choices: ["Batman", "Spider-Man", "Iron Man", "Deadpool"],
+        answer: "Iron Man"
+    },
+    {
+        question: "What is Batman's alias?",
+        choices: ["Tony Stark", "Bruce Wayne", "Clark Kent", "Steven Strange"],
+        answer: "Bruce Wayne"
+    },
+    {
+        question: "Which superhero is not part of the Fantastic 4?",
+        choices: ["The Hulk", "Mr. Fantastic", "Human Torch", "Susan Storm"],
+        answer: "The Hulk"
+    },
+    {
+        question: "Which newspaper does Peter Parker work for?",
+        choices: ["New Daily Globe", "The Daily Planet", "The Daily News", "The Daily Bugle"],
+        answer: "The Daily Bugle"
+    },
+    {
+        question: "Wolverine's claws are made of",
+        choices: ["Titanium", "Adamantium", "Vibranium", "Aluminum"],
+        answer: "Adamantium"
+    },
+    {
+        question: "What item is not part of Batman's aresenal?",
+        choices: ["Batmobile", "Grappling Hook", "Power Ring", "Utility Belt"],
+        answer: "Power Ring"
+    },
+    {
+        question: "What Superhero team includes a sentient tree creature?",
+        choices: ["Teen Titans", "Guardians Of The Galaxy", "Suicide Squad", "The Justice League"],
+        answer: "Guardians Of The Galaxy"
+    },
+    {
+        question: "Question8",
+        choices: ["a", "b", "c", "d"],
+        answer: "Answer8"
+    },
+    {
+        question: "Question9",
+        choices: ["a", "b", "c", "d"],
+        answer: "Answer9"
+    },
+    {
+        question: "Question10",
+        choices: ["a", "b", "c", "d"],
+        answer: "Answer10"
     }
+];
 
+//default game values
+var counter = 20;
+var currentQuestion = 0;
+var correct = 0;
+var incorrect = 0;
+var timer;
+
+
+//function to go to the next question
+function nextQuestion(){
+    currentQuestion++;
+    showQuestion();
 }
 
-function guessChecker(){
-    if(trivia.choices === trivia.answers){
-        alert("correct")
-    } else {
-        alert("Wrong")
+//make a counter
+function timeStop(){
+    clearInterval(timer);
+    incorrect++;
+    nextQuestion();
+}
+
+function countDown(){
+    counter--;
+    $(".timer").html("<h5>" + counter + "<br>Seconds remaining</h5>");
+    if(counter ===0){
+        timeStop();
     }
 }
 
-function timerRun(){
-    //if(trivia.timer > -1 && trivia.currentSet < Object.keys(trivia.questions).length){
-        trivia.timer--;
-        $(".timer").text(trivia.timer + " Seconds");
-    //}
+//make a function to load question to the page
+function showQuestion(){
+    counter = 20;
+    timer = setInterval(countDown, 1000);
+    var question = trivia[currentQuestion].question;
+    var choices = trivia[currentQuestion].choices;
+
+    $(".question").html("<h3>" + question + "</h3>");
+    $(".timer").html("<h5>" + counter + "<br>Seconds remaining</h5>");
+    $(".choices").html(showChoices(choices));
 }
-timeRun()
 
-
-})
+function showChoices(choices){
+    var result = "";
+    for(var i = 0; i < choices.length; i++){
+        result = result + ("<p class='btn btn-primary col-sm m-2' data-answer='choices[i]'>" + choices[i] + "</p>");
+    }
+    return result;
+}
+showQuestion();
